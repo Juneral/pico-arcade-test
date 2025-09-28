1bce6ef60a0bd124b2fa54d161050f947aca7f08
Juneral <30858934+Juneral@users.noreply.github.com>
2025-09-28 14:14:32 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -10,15 +10,16 @@ namespace elfshield {
     /**
      * Single LED set Light
     // */
-    // //% blockId="ELF_Display_singleLEDSet" 
-    // //% weight=22
-    // //% blockGap=10
-    // //% shim=elfshield::singleLEDSet
-    // export function singleLEDSet(nane: isOn: swStatus): void {
-    //     return;
-    // }
+   //% blockId="ELF_Display_singleLEDSet" block="Single LED|%pinNum| set |%swStatus"
+    //% weight=22
+    //% blockGap=10
+    //% shim=elfshield::singleLEDSet
+    export function singleLEDSet(pinNum: number, isOn: swStatus): void {
+        return;
+    }
 
 }
+
 
 
 
