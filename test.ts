8875b2f1ea3e4929fc2e06948ff5f519f3644a51
Juneral <30858934+Juneral@users.noreply.github.com>
2025-09-28 10:36:58 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,13 +1,21 @@
+enum swStatus {
+    //% block="On"
+    On = 1,
+    //% block="Off"
+    Off = 0
+}
+
 namespace test {
 
     /**
      * Single LED set Light
     */
-    //% blockId="ELF_Display_singleLEDSet" block="Single LED|%pinNum| set |%swStatus"
+    //% blockId="ELF_Display_singleLEDSet" block="Single LED set |%swStatus"
     //% weight=22
     //% blockGap=10
     //% shim=elfshield::singleLEDSet
     export function singleLEDSet(isOn: number): void {
         return;
     }
+
 }
