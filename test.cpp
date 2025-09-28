6a28f1f4f4fb4df6b3fc44a56348269cfefbfb39
Juneral <30858934+Juneral@users.noreply.github.com>
2025-09-28 14:53:52 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -4,13 +4,14 @@ namespace elfshield {
     /*
     **Single LED module V1.0.
     */
-//% block="digital write|pin %name|to %value=toggleHighLow"
+//%
 void digitalWrite(DigitalInOutPin name, bool value) {
     PINOP(setDigitalValue(value));
 }
 
 
 }
+
 
 
 
